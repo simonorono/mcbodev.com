c5cf58abf8574a848d61469fb02244fe337a4d2b
Simón Oroño <simonorono@protonmail.com>
2021-06-10 19:32:44 -0400

    Start Vite + Tailwind + Vue project
    Also added a favicon.

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Index/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Index from './components/Index.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Index
+  }
+})
+</script>
