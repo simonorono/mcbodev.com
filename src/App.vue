fc6e69ad35233fa489f2b0877ed4f6438a0d8f9a
Simón Oroño <simonorono@protonmail.com>
2021-06-10 21:48:17 -0400

    Remove index references from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Header from './components/Header.vue'
-import Index from './components/Index.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     Header,
-    Index,
   }
 })
 </script>
