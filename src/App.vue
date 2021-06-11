3ec5749301dfb93e3e6f509b3e48b581743ae650
Simón Oroño <simonorono@protonmail.com>
2021-06-10 20:35:58 -0400

    Add header to the page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
-<template>
-  <Index/>
-</template>
-
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Header from './components/Header.vue'
 import Index from './components/Index.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
+    Header,
     Index
   }
 })
 </script>
+
+<template>
+  <div class="min-h-screen max-w-screen-lg mx-auto">
+    <Header />
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: 'Header'
+})
+</script>
+
+<template>
+  <div class="mt-10 mx-2 sm:mt-20 lg:mx-0">
+    <div class="flex flex-col sm:flex-row sm:justify-between sm:items-baseline">
+      <h1 class="text-5xl font-bold">Simón Oroño</h1>
+      <h2 class="font-medium">Computer Scientist & Software Developer</h2>
+    </div>
+    <hr class="mt-2">
+  </div>
+</template>
