240bd49dc4e0d13e979443eb07d47ed08d7d2c3c
Simón Oroño <simonorono@protonmail.com>
2021-06-12 10:49:34 -0400

    Add links to projects cards
    * Added heroicons lib

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { CodeIcon, GlobeIcon } from '@heroicons/react/outline'
+
 import Tag from './Tag'
 
 interface Props {
@@ -6,13 +8,31 @@ interface Props {
 }
 
 export default function ProjectCard(props: Props) {
-  const { name, description, tags } = props.project
+  const { name, description, tags, url, vcs } = props.project
 
   return (
-    <div className="border p-2 space-y-4">
+    <div className="flex flex-col border p-2 space-y-4">
       <h3 className="text-2xl font-medium">{name}</h3>
 
-      <p className="">{description}</p>
+      <div className="space-y-3 sm:space-y-0">
+        <a
+          className="flex items-center space-x-1"
+          href={url}
+        >
+          <GlobeIcon className="h-5 w-5" />
+          <span className="underline"> Website</span>
+        </a>
+
+        <a
+          className="flex items-center space-x-1"
+          href={vcs}
+        >
+          <CodeIcon className="h-5 w-5" />
+          <span className="underline"> Repository</span>
+        </a>
+      </div>
+
+      <p className="flex-grow pt-2">{description}</p>
 
       <div className="space-y-2 relative bottom-0">
         {tags.map(tag => (
