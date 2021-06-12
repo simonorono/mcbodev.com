434143a40e7a78d79fb312890f67ff5359717986
Simón Oroño <simonorono@protonmail.com>
2021-06-12 09:08:22 -0400

    Add React Helmet to improve meta updates

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,28 +1,36 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import Template from './Template'
 
 export default function Index() {
   return (
-    <Template
-      h1="Simón Oroño"
-      h2="Computer Scientist & Software Developer"
-    >
-      <div className="mt-14 text-xl sm:max-w-lg space-y-8">
-        <p>
-          I'm a computer scientist and software developer from Maracaibo, Venezuela.
-          I graduated with honors from La Universidad del Zulia.
-        </p>
+    <>
+      <Helmet>
+        <title>Simón Oroño — mcbodev.com</title>
+        <meta name="description" content="Simón Oroño. Computer scientist and software developer."/>
+      </Helmet>
 
-        <p>
-          I work as a full stack web developer, but most of my experience is with
-          back-end development, databases and SQL.
-        </p>
+      <Template
+        h1="Simón Oroño"
+        h2="Computer Scientist & Software Developer"
+      >
+        <div className="mt-14 text-xl sm:max-w-lg space-y-8">
+          <p>
+            I'm a computer scientist and software developer from Maracaibo, Venezuela.
+            I graduated with honors from La Universidad del Zulia.
+          </p>
 
-        <p>
-          My academic interests are programming language theory, graph theory and
-          discrete mathematics.
-        </p>
-      </div>
-    </Template>
+          <p>
+            I work as a full stack web developer, but most of my experience is with
+            back-end development, databases and SQL.
+          </p>
+
+          <p>
+            My academic interests are programming language theory, graph theory and
+            discrete mathematics.
+          </p>
+        </div>
+      </Template>
+    </>
   )
 }
