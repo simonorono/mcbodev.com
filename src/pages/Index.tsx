42c28f1a5dc30806ff9a3b28c1203abc0d453f8c
Simón Oroño <simonorono@protonmail.com>
2021-06-12 08:13:20 -0400

    Started migration of components from Vue to React

---
+++ src/pages/Index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div className="mt-14 text-xl sm:max-w-lg space-y-8">
+      <p>
+        I'm a computer scientist and software developer from Maracaibo, Venezuela.
+        I graduated with honors from La Universidad del Zulia.
+      </p>
+
+      <p>
+        I work as a full stack web developer, but most of my experience is with
+        back-end development, databases and SQL.
+      </p>
+
+      <p>
+        My academic interests are programming language theory, graph theory and
+        discrete mathematics.
+      </p>
+    </div>
+  )
+}
