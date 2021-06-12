144a76fb8f67da15ebca98e077ee1e7e4243c07a
Simón Oroño <simonorono@protonmail.com>
2021-06-11 21:05:41 -0400

    Load fonts from the same site
    Avoid network impact of loading from another domain

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import './index.css'
+import './assets/Inter Web/inter.css'
 
 const app = createApp(App)
 
