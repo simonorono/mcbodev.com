39e2dd73c964137aa77ccb885f23f4a76fb21f97
Simón Oroño <simonorono@protonmail.com>
2021-06-16 20:14:17 -0400

    Make "go back" button bold on hover

--- src/components/BackToIndex.tsx
+++ src/components/BackToIndex.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function BackToIndex() {
   return (
     <div>
-      <Link to="/">
+      <Link to="/" className="hover:font-bold">
         <span>&lt;- </span><span className="underline">go back</span>
       </Link>
     </div>
