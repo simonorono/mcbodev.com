eb7f6d3b2c17f332139948ee874cdccbc8c3b5f6
Simón Oroño <simonorono@protonmail.com>
2021-06-16 20:42:49 -0400

    Add footer with link to the page source code

--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react'
 import Header from '../components/Header'
+import { NewTabLink } from '../components/NewTabLink'
 
 interface Props {
   h1: string,
@@ -12,14 +13,26 @@ export default function Template(props: Props) {
 
   return (
     <div className="max-w-screen-lg mx-auto">
-      <div className="mx-2 lg:mx-0">
+      <div className="mx-2 lg:mx-0 h-screen flex flex-col">
         <header>
           <Header {...{ h1, h2 }} />
         </header>
 
-        <main>
+        <main className="flex-grow">
           {children}
         </main>
+
+        <footer>
+          <hr className="mt-8" />
+
+          <div className="my-4 text-right">
+            <NewTabLink
+              href={"https://github.com/simonorono/mcbodev.com"}
+              name="Source code for this page."
+              className="text-sm"
+            />
+          </div>
+        </footer>
       </div>
     </div>
   )
