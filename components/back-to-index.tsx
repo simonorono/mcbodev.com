96dced76eb1c6dadf7fda825044a0638c97508ce
Simón Oroño <simonorono@protonmail.com>
2021-10-31 13:24:40 -0400

    Add top margin for the "go back" button on mobile screen size

--- components/back-to-index.tsx
+++ components/back-to-index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function BackToIndex() {
   return (
-    <div>
+    <div className="mt-8 sm:mt-0">
       <Link href="/" passHref>
         <a className="hover:font-bold">
           <span>&lt;- </span><span className="underline">go back</span>
