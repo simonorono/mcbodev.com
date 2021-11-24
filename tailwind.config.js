32fee0cf8f16d1e81f1b42cf4359fe48f607240d
Simón Oroño <simonorono@protonmail.com>
2021-11-24 07:19:19 -0400

    Remove Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,6 @@ module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
-      }
-    },
-  },
   variants: {
     extend: {},
   },
